80455624fedd7adbe7c15beea4deda8e84faf56c
quynhnguyen2612 <vanquynh336@gmail.com>
2023-12-08 20:38:17 +0700

    Access modifier

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        TestCircle Circle = new TestCircle();
+        System.out.println( Circle.getRadius());
+        System.out.println(Circle.getArea());
+    }
+}
---
+++ src/TestCircle.java
@@ -0,0 +1,19 @@
+public class TestCircle {
+    private double  radius = 1.0;
+    private String color = "red";
+
+    public TestCircle() {
+    }
+
+    public TestCircle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getArea() {
+        return color;
+    }
+}
